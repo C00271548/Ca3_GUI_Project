113a985c0bb43d869c5fb440788ca7056e6f4ad2
Gabor Major <113538041+C00271548@users.noreply.github.com>
2023-02-07 09:52:17 +0000

    Update GUIDriver.java

--- GUIDriver.java
+++ GUIDriver.java
@@ -6,15 +6,23 @@ import java.lang.Thread;
 public class GUIDriver
 {
 	private static LoginGUI loginGUI;
+	private static SignUpGUI signUpGUI;
 	
 	public static void main(String[] args)
     {
 		renderLoginGUI();
+		renderSignUpGUI();
+		loginGUI.setVisible(true);
 		while (true)
 		{
 			try
 			{
 				System.out.println(loginGUI.commandInt);
+				if (loginGUI.commandInt == 1)
+				{
+					loginGUI.setVisible(false);
+					signUpGUI.setVisible(true);
+				}
 				Thread.sleep(100);
 			}
 			catch (InterruptedException e)
@@ -31,16 +39,16 @@ public class GUIDriver
 		loginGUI.pack();
 		loginGUI.setResizable(false);
 		loginGUI.setLocation(500,400);
-		loginGUI.setVisible(true);
+		loginGUI.setVisible(false);
 	}
 
 	public static void renderSignUpGUI()
 	{
-		SignUpGUI signUpGUI = new SignUpGUI("Sign Up");
+		signUpGUI = new SignUpGUI("Sign Up");
 		signUpGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		signUpGUI.pack();
 		signUpGUI.setResizable(false);
 		signUpGUI.setLocation(500,400);
-		signUpGUI.setVisible(true);
+		signUpGUI.setVisible(false);
 	}
 }
